5dcfcbb5242db78f5735fb52026260c536d4113f
David May <David.May@efi.com>
2021-08-29 23:16:48 -0700

    Updated file header 'readme'

--- tac-mkrecital.sh
+++ tac-mkrecital.sh
@@ -61,6 +61,7 @@ $plurl
 Tony's Acoustic Challenge (TAC) can be found at https://tonypolecastro.com.
 
 This video and the chapters markers below were generated using youtube-dl and ffmpeg. The script can be found at https://github.com/davfive/ytutils/blob/main/tac-mkrecital.sh.
+
 Challenges:
 EOCAT
 
