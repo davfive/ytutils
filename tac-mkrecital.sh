6b7186ef78894528d8b1e85d4f99f07f6949289d
David May <David.May@efi.com>
2021-08-29 22:27:33 -0700

    Incrementally add videos to final so that I can grab a more accurate chapter start timestamp

--- tac-mkrecital.sh
+++ tac-mkrecital.sh
@@ -10,71 +10,67 @@ plurl=$1
 
 ffmpeg="ffmpeg -hide_banner -loglevel error"
 ffmpeg_drawops="x=w-tw-10:y=10:fontsize=24:fontcolor=white:line_spacing=10:box=1:boxborderw=10:boxcolor=black@0.4"
+ffduration="ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 -sexagesimal"
 
-# =============================================================================
 # Setup playlist-# dir
 pldir=playlist; n=1
 while [[ -d "$pldir-$n" ]] ; do n=$(($n+1)) ; done
-pldir=$pldir-$n
+pldir=$pldir-5 #$n
 mkdir -p "$pldir"
 
-# =============================================================================
 # Get Playlist Info
+echo; echo "## Getting playlist info ..."
 pljson=$pldir/playlist.json
 (set -x ; youtube-dl --skip-download --flat-playlist -J $plurl > $pljson)
 
-# =============================================================================
 # Download Playlist Videos
-(set -x ; youtube-dl --abort-on-error -f mp4 -o "$pldir/%(title)s.orig.%(ext)s" $plurl)
+echo; echo "## Downloading playlist videos ..."
+#(set -x ; youtube-dl --abort-on-error -f mp4 -o "$pldir/%(title)s.%(ext)s" $plurl)
 
-# =============================================================================
-# Annotate Videos with Challenge # and Title
-(cd $pldir; ls TAC* | while IFS= read f; do
-  fname=$(basename "$f" .orig.mp4)
-  annfile="$fname.annotated.mp4"
+# Video files
+pltitle="$(jq -r '.title' $pljson)"
+plvideo="$pltitle.mp4"          # Final video
+plcurrvid="$pltitle.curr.mp4"   # Current video (with previous concats)
+plnextvid="$pltitle.next.mp4"   # Nextvideo to concatenate with
+plcatfiles="plcatfiles.txt"     # File to concatenate
+
+pldesc="$pltitle.desc.txt"
+cat <<EOCAT > "$pldir/$pldesc"
+A monthly TAC Daily Challenge "Recital" from my recordings
+From Playlist: $pltitle
+$plurl
+
+Challenges:
+EOCAT
+
+# Foreach video, annotate, concatenate, and mark start of chapter
+plchapstart=00:00
+(cd $pldir; ls TAC*.mp4 | while IFS= read f; do
+  echo; echo "## Adding '$f' ..."
+
+  # Filename format: TAC YYYYMMW#D# - <challenge-name> @<#bpm>.mp4
+  fname=$(basename "$f" .mp4)
   tacnum=$(echo "$fname" | cut -f1 -d- | xargs)
   tacname=$(echo "$fname" | cut -f2 -d- | xargs)
 
-  # Why < /dev/null? https://unix.stackexchange.com/a/36411
-  #  prevents ffmpeg from reading from standard input which makes it not work in loops
-  (set -x ; < /dev/null $ffmpeg -i "$f" -vf "drawtext=text='$tacnum
-$tacname':$ffmpeg_drawops" -c:a copy "$annfile")
+  # Create Annotated Video to concatenate next
+  #   Why < /dev/null? https://unix.stackexchange.com/a/36411
+  #   prevents ffmpeg from reading from standard input which makes it not work in loops
+  (set -x; < /dev/null $ffmpeg -i "$f" -vf "drawtext=text='$tacnum
+$tacname':$ffmpeg_drawops" -c:a copy "$plnextvid")
+
+  # Concatenate to end of video (and rotate video files)
+  [[ -f "$plcurrvid" ]] && echo "file '$plcurrvid'" >> $plcatfiles
+  [[ -f "$plnextvid" ]] && echo "file '$plnextvid'" >> $plcatfiles
+  (set -x; < /dev/null $ffmpeg -safe 0 -f concat -i "$plcatfiles" -c copy "$plvideo")
+  rm -f "$plcurrvid" "$plnextvid" "$plcatfiles"
+  mv "$plvideo" "$plcurrvid" # setup for next loop
+
+  # Track chapter starts (previous end is this round's start, then setup for next loop)
+  echo "$plchapstart $fname" >> "$pldesc"
+  plchapstart=$($ffduration "$plcurrvid" | cut -d. -f1 | cut -d: -f2-) # 0:00:00.000000 to 00:00
 done)
 
-# =============================================================================
-# Create list of annotated videos to combine
-pljoin=$pldir/pljoin.txt
-rm -f $pljoin
-(cd $pldir; ls TAC*annotated.mp4 | while IFS= read f; do 
-  echo "file '$f'" >> $(basename $pljoin);
-done)
-
-# =============================================================================
-# Combine videos
-pltitle="$(jq -r '.title' $pljson)"
-plvideo="$pldir/$pltitle.mp4"
-(set -x ; $ffmpeg -safe 0 -f concat -i $pljoin -c copy "$plvideo")
-
-# =============================================================================
-# Create chapter lines for video description
-pltitles=$pldir/pltitles.txt
-pldurations=$pldir/pldurations.txt
-pltimes=$pldir/pltimes.txt
-plchapters=$pldir/$pltitle.chapters.txt
-jq -r '.entries[].title' $pljson > $pltitles
-jq -r '.entries[].duration' $pljson > $pldurations
-t=0; while IFS= read dur; do 
-  # YouTube chapter format: 0:12, 4:03, ... (use single digits < 10)
-  echo $(date -u -r $t +"%M:%S" | sed "s/^0\([0-9]\)/\1/") >> $pltimes; 
-  t=$(($t + $dur));
-done < $pldurations
-paste -d' - ' $pltimes $pltitles > $plchapters
-rm $pltitles $pldurations $pltimes
-
-# =============================================================================
-# Report results
-echo "====================================="
-echo "Playlist: $(jq -r '.title' $pljson)"
-echo "Video: $plvideo"
-echo "Chapters:"
-cat $plchapters
+mv "$pldir/$plcurrvid" "$pldir/$plvideo" # store final video
+echo
+(set -x; cat "$pldir/$pldesc")
